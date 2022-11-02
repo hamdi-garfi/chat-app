844fdd278c5e1f55006053798a0ddbff174fd2e6
Your Name <garfi.hamdi@gmail.com>
2022-11-02 01:04:37 +0100

    correction bug

--- docker/conf/postgres/initdb.sh
+++ docker/conf/postgres/initdb.sh
@@ -1,7 +1,35 @@
-#!/bin/bash
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB"  <<-EOSQL
-     #!/bin/sh
-psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
-psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER '$DB_USER'"
+#!/usr/bin/env bash
+psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB?sslmode=disable" <<-EOSQL
+
+
+CREATE TABLE IF NOT EXISTS connexion (
+  id serial primary key,
+  user_id serial NOT NULL,
+  datetime timestamp NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE IF NOT EXISTS message (
+  id serial primary key,
+  content text NOT NULL,
+  created_at timestamp NOT NULL,
+  user_id serial NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS "user"  (
+  id serial primary key,
+  name varchar(255) NOT NULL,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  created_at timestamp NOT NULL
+);
+
+TRUNCATE connexion, message,"user" RESTART IDENTITY;
 
 EOSQL
